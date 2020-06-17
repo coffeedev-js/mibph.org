57812700b4b74fbe25ba5c95cdf843db5f8b876e
xtakoyakii <50556930+xtakoyakii@users.noreply.github.com>
2020-06-17 17:48:00 +0200

    New content! + Update UI upgrades

---
+++ JS/mainnegative.js
@@ -0,0 +1,61 @@
+
+
+var seal = document.getElementById("seal_normal");
+var seal_alien = document.getElementById("seal_alien");
+var seal_corrupted = document.getElementById("seal_corrupted");
+var seal_holy = document.getElementById("seal_holy");
+
+
+document.querySelectorAll('.seal_image').forEach(item => {
+  item.addEventListener('mouseover', function(){
+      this.style = "width: 270px;"
+    })
+  })
+
+document.querySelectorAll('.seal_image').forEach(item => {
+  item.addEventListener('mouseout', function(){
+      this.style = "none;"
+    })
+  })
+
+seal.addEventListener("click", function(){
+  var audio = new Audio("Media/mibph_corrupted.mp3");
+  audio.play();
+})
+seal_alien.addEventListener("click", function(){
+  var audio = new Audio("Media/mibph_corrupted.mp3");
+  audio.play();
+})
+seal_corrupted.addEventListener("click", function(){
+  var audio = new Audio("Media/mibph_corrupted.mp3");
+  audio.play();
+})
+seal_holy.addEventListener("click", function(){
+  var audio = new Audio("Media/mibph_corrupted.mp3");
+  audio.play();
+})
+
+/*Mobilgerate weiterleiten*/
+ if (window.screen.width < 1000) {
+  window.location = 'm.mibph.org';
+}
+
+/*to top button*/
+var t1 = 0;
+window.onscroll = scroll1;
+
+function scroll1() {
+  var toTop = document.getElementById('toTop');
+  window.scrollY > 0 ? toTop.style.display = 'Block' : toTop.style.display = 'none';
+}
+
+function smoothscroll() {
+  var y1 = window.scrollY;
+  y1 = y1 - 1000;
+  window.scrollTo(0, y1);
+  if (y1 > 0) {
+    t1 = setTimeout("smoothscroll()", 100);
+  } else {
+    clearTimeout(t1);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mibph.org
-A repurposed repo to mibph.org! A simple frontend for a Bot that will be sending reaction images of Seals when asked for. The frontend itself consists out of a soundboard of Seals.
+>A repurposed repo to mibph.org! A simple Landingpage for a Bot that will be sendingreaction images of Seals when asked for. It has other cool functions such as a seal Soundboard or random facts about seals!
+
+## To do list
+
+1. Build Mobile version of website, redirect mobile users to it
+2. Change Background image with larger version for sites that are longer, so there would be for repeating the background
+3. Randomize background on load
+4. Add "Random Seal facts"
+5. Add "About"
+6. Add a link to "Takolabs"
+7. Add "Other websites"
+8. Add "Currently in progress" page
+9. Build a Landing page for the Bot, put the current homepage into a seperate tab
+10. Route 404s to customized page
