470134bdd22e37f84074c3c2b551a1746d8fbfac
xtakoyakii <50556930+xtakoyakii@users.noreply.github.com>
2020-06-22 01:47:18 +0200

    Seal DB update

--- facts.php
+++ facts.php
@@ -13,10 +13,10 @@
       <div id='cssmenu'>
         <ul>
            <li class='active'><a href='https://www.mibph.org/'> Home <i class="fas fa-home"></i></a></li>
-           <li><a href='#'><i class="fas fa-random"></i> Random Seal facts</a></li>
-           <li><a href='#'><i class="fas fa-address-card"></i> About this website</a></li>
-           <li><a href='#'><i class="fas fa-globe-europe"></i> Other Websites</a></li>
-           <li><a href="#"><i class="fas fa-flask" id="labs"></i> Takolabs</a></li>
+           <li><a href='https://www.mibph.org/facts.html'><i class="fas fa-random"></i> Random Seal facts</a></li>
+           <li><a href='https://www.mibph.org/comingsoon.html'><i class="fas fa-address-card"></i> About this website</a></li>
+           <li><a href='https://www.mibph.org/comingsoon.html'><i class="fas fa-globe-europe"></i> Other Websites</a></li>
+           <li><a href="https://www.mibph.org/comingsoon.html"><i class="fas fa-flask" id="labs"></i> Takolabs</a></li>
         </ul>
       </div>
         <div class="sealcontainment animate__animated animate__fadeInUp">
@@ -25,15 +25,22 @@
           <div class="container">
           <div class="center">
         </div>
-        <button class="btn">
+        <button class="btn" onClick="window.location.reload();">
              <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
                <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
                <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
              </svg>
-             <a href="https://www.mibph.org/facts.php"><span> Draw new</span></a>
+            <span> Draw new</span>
         </button>
          </div>
         </div>
+        <br>
+        <p>All Seal facts are stored at our Seal Database, which pulls facts from various websites.
+        <br>
+        While we try to fact check each quote, please understand thaat newer ones might not have been checked yet.
+        <br>
+        A list with websites that we pull the facts from into our Database will be coming soon.
+      </p>
         <footer class="animate__animated animate__fadeInUp">
           <p>Front- and backend developed by xTakoyakii© and RaccoonGuy©</p>
           <p>xTakoyakii© and RaccoonGuy© fall under the Copyright of Takolabs©</p>
--- getquote.php
+++ getquote.php
@@ -1,11 +1,13 @@
 <?php
-    $quotes[] = "quote 1";
-    $quotes[] = "quote 2";
-    $quotes[] = "quote 3";
-    $quotes[] = "quote 4";
-    $quotes[] = "quote 5";
-    $quotes[] = "quote 6";
-
+    $quotes[] = "Seals are part of the pinniped order of marine mammals, which also includes sea lions, walruses and fur seals. There are 33 species of pinnipeds in the world and all are believed to have evolved from once terrestrial otter-like creatures.";
+    $quotes[] = "Seals prefer cold sea waters and are primarily found in the Arctic and Antarctic regions.";
+    $quotes[] = "In the wild, seals can live for up to 30 years with females boasting a higher life expectancy than males.";
+    $quotes[] = "Some seal species, like the Leopard Seal, feed off fellow seals! Most, however, feed off crustaceans, fish and seabirds (if they can catch them).";
+    $quotes[] = "The Crabeater Seal, which can be spotted on an Antarctic cruise, boasts the largest population of all the seal species. Estimates put their numbers at between 2 and 75 million. individuals, worldwide. As cute and friendly as they may look, seals can actually be fervently territorial and quite aggressive.";
+    $quotes[] = "Seals can sleep underwater and usually only come on land to escape predators like whales and sharks, as well as to mate, give birth, feed and moult.";
+    $quotes[] = "Some seals can dive up to 900m in depth and some can hold their breath underwater for up to 2 hours!"
+    $quotes[] = "A seal mum’s milk can be up to 50% fat, and pups can put on 2kgs a DAY. That should make you feel a little better about your Christmas and New Year calorific splurge!"
+    $quotes[] ="Seals have been hunted throughout history, with their fat and fur being highly coveted. Although a great majority of species are not endangered – the Galapagos Fur Seal is one of the exceptions – seals are still a highly protected mammal in most parts of the world. Ove the last century, various species have gone extinct, including the Caribbean Monk Seal and Japanese Sea Lion."
     srand ((double) microtime() * 1000000);
     $randomquote = rand(0,count($quotes)-1);
     echo "<p>" . $quotes[$randomquote] . "</p>";
