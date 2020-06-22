85233d3c3ef2eb1f69f807a1253f76877bbf705b
xtakoyakii <50556930+xtakoyakii@users.noreply.github.com>
2020-06-22 10:20:46 +0200

    Updated Seal algorythm

--- facts.php
+++ facts.php
@@ -13,7 +13,7 @@
       <div id='cssmenu'>
         <ul>
            <li class='active'><a href='https://www.mibph.org/'> Home <i class="fas fa-home"></i></a></li>
-           <li><a href='https://www.mibph.org/facts.html'><i class="fas fa-random"></i> Random Seal facts</a></li>
+           <li><a href='https://www.mibph.org/facts.php'><i class="fas fa-random"></i> Random Seal facts</a></li>
            <li><a href='https://www.mibph.org/comingsoon.html'><i class="fas fa-address-card"></i> About this website</a></li>
            <li><a href='https://www.mibph.org/comingsoon.html'><i class="fas fa-globe-europe"></i> Other Websites</a></li>
            <li><a href="https://www.mibph.org/comingsoon.html"><i class="fas fa-flask" id="labs"></i> Takolabs</a></li>
@@ -35,17 +35,14 @@
          </div>
         </div>
         <br>
-        <p>All Seal facts are stored at our Seal Database, which pulls facts from various websites.
-        <br>
-        While we try to fact check each quote, please understand thaat newer ones might not have been checked yet.
-        <br>
-        A list with websites that we pull the facts from into our Database will be coming soon.
-      </p>
         <footer class="animate__animated animate__fadeInUp">
           <p>Front- and backend developed by xTakoyakii© and RaccoonGuy©</p>
           <p>xTakoyakii© and RaccoonGuy© fall under the Copyright of Takolabs©</p>
           <p>Mibph.org© | 2020</p>
           <a href="https://www.youtube.com/watch?v=YFZJeap_K3U" target="_blank"><p>Imprint & Privacy Policy</p></a>
+          <p class="smol">All Seal facts are stored at our Seal Database, which pulls facts from various websites.</p>
+          <p class="smol">While we try to fact check each quote, please understand that newer ones might not have been checked yet.</p>
+          <p class="smol">A list with websites that we pull the facts from into our Database will be coming soon.</p>
         </footer>
       <script type="text/javascript" src="JS/main.js"></script>
       <img id="toTop" src="https://alex-web.dev/files/public-docs/arrow.png" onclick="smoothscroll()" title="Go To Top">
--- getquote.php
+++ getquote.php
@@ -5,9 +5,9 @@
     $quotes[] = "Some seal species, like the Leopard Seal, feed off fellow seals! Most, however, feed off crustaceans, fish and seabirds (if they can catch them).";
     $quotes[] = "The Crabeater Seal, which can be spotted on an Antarctic cruise, boasts the largest population of all the seal species. Estimates put their numbers at between 2 and 75 million. individuals, worldwide. As cute and friendly as they may look, seals can actually be fervently territorial and quite aggressive.";
     $quotes[] = "Seals can sleep underwater and usually only come on land to escape predators like whales and sharks, as well as to mate, give birth, feed and moult.";
-    $quotes[] = "Some seals can dive up to 900m in depth and some can hold their breath underwater for up to 2 hours!"
-    $quotes[] = "A seal mum’s milk can be up to 50% fat, and pups can put on 2kgs a DAY. That should make you feel a little better about your Christmas and New Year calorific splurge!"
-    $quotes[] ="Seals have been hunted throughout history, with their fat and fur being highly coveted. Although a great majority of species are not endangered – the Galapagos Fur Seal is one of the exceptions – seals are still a highly protected mammal in most parts of the world. Ove the last century, various species have gone extinct, including the Caribbean Monk Seal and Japanese Sea Lion."
+    $quotes[] = "Some seals can dive up to 900m in depth and some can hold their breath underwater for up to 2 hours!";
+    $quotes[] = "A seal mum’s milk can be up to 50% fat, and pups can put on 2kgs a DAY. That should make you feel a little better about your Christmas and New Year calorific splurge!";
+    $quotes[] ="Seals have been hunted throughout history, with their fat and fur being highly coveted. Although a great majority of species are not endangered – the Galapagos Fur Seal is one of the exceptions – seals are still a highly protected mammal in most parts of the world. Ove the last century, various species have gone extinct, including the Caribbean Monk Seal and Japanese Sea Lion.";
     srand ((double) microtime() * 1000000);
     $randomquote = rand(0,count($quotes)-1);
     echo "<p>" . $quotes[$randomquote] . "</p>";
